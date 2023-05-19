bee71da5624e99c2fd5fe6730f03a60e264e88d3
Igor Pereira <igor@gargantas.org>
2023-05-18 21:32:17 -0300

    More AuthContext code refactoring

--- src/context/Auth/AuthContext.tsx
+++ src/context/Auth/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 import {
   FC,
   ReactNode,
@@ -6,137 +6,38 @@ import {
   useContext,
   useEffect,
   useMemo,
-  useReducer,
-} from "react";
+  useReducer
+} from 'react';
 
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 
-import { auth } from "@/firebase";
-import { googleProvider } from "@/firebase/providers";
+import { auth } from '@/firebase';
+import { googleProvider } from '@/firebase/providers';
 import {
   checkUserDocExists,
   createCredentialsUser,
   createEmptyUserDoc,
   signInCredentials,
-  signInPopup,
-} from "@/util/user";
-import { initialActionsState, initialState, reducer } from "./state";
-import { ActionsState } from "./types";
+  signInPopup
+} from '@/util/user';
+import { initialActionsState, initialState, reducer } from './state';
+import { ActionsState } from './types';
+import { useAuthState } from '@/hooks/useAuthState';
 
 const AuthContext = createContext(initialState);
 const AuthActionsContext = createContext(initialActionsState);
 
 export const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const router = useRouter();
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const actionsState = useMemo<ActionsState>(
-    () => ({
-      dispatch,
-      async signUp(email: string, password: string) {
-        dispatch({ type: "LOADING" });
-        try {
-          const user = await createCredentialsUser(auth, email, password);
-          console.log("user: " + user.emailVerified);
-          const isUserCreated = await createEmptyUserDoc(user);
-
-          if (!isUserCreated)
-            return dispatch({
-              type: "REGISTER_ERROR",
-              payload: "Failed to create user document.",
-            });
-
-          console.log("User document created successfully.");
-          router.prefetch("/dashboard");
-          router.push("/dashboard");
-        } catch (error) {
-          dispatch({
-            type: "REGISTER_ERROR",
-            payload: (error as Error).message,
-          });
-        }
-      },
-      async signIn(email: string, password: string) {
-        dispatch({ type: "LOADING" });
-        try {
-          const user = await signInCredentials(auth, email, password);
-          console.log("user: " + user.emailVerified);
-          const userExists = await checkUserDocExists(user.uid);
-
-          if (!userExists) {
-            const isUserCreated = await createEmptyUserDoc(user);
-
-            if (!isUserCreated)
-              return dispatch({
-                type: "REGISTER_ERROR",
-                payload: "Failed to create user document.",
-              });
-
-            dispatch({ type: "REGISTER_SUCCESS", payload: user });
-          } else dispatch({ type: "LOGIN_SUCCESS", payload: user });
-
-          router.prefetch("/dashboard");
-          router.push("/dashboard");
-        } catch (error) {
-          dispatch({
-            type: "LOGIN_ERROR",
-            payload: (error as Error).message,
-          });
-        }
-      },
-
-      async signInWithGoogle() {
-        dispatch({ type: "LOADING" });
-        try {
-          const user = await signInPopup(auth, googleProvider);
-          const userExists = await checkUserDocExists(user.uid);
-
-          if (!userExists) {
-            const isUserCreated = await createEmptyUserDoc(user);
-
-            if (!isUserCreated)
-              return dispatch({
-                type: "REGISTER_ERROR",
-                payload: "Failed to create user document.",
-              });
-
-            dispatch({ type: "REGISTER_SUCCESS", payload: user });
-          } else
-            dispatch({
-              type: "LOGIN_SUCCESS",
-              payload: user,
-            });
-
-          router.prefetch("/dashboard");
-          router.push("/dashboard");
-        } catch (error) {
-          dispatch({ type: "LOGIN_ERROR", payload: (error as Error).message });
-        }
-      },
-
-      async signOut() {
-        dispatch({ type: "LOADING" });
-        try {
-          await signOut(auth);
-          router.push("/");
-        } catch (error) {
-          dispatch({
-            type: "LOGOUT_ERROR",
-            payload: (error as Error).message,
-          });
-        }
-      },
-    }),
-    [dispatch]
-  );
+  const [state, actionsState, dispatch] = useAuthState();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) dispatch({ type: "LOGIN_SUCCESS", payload: user });
-      else dispatch({ type: "LOGOUT_SUCCESS" });
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) dispatch({ type: 'LOGIN_SUCCESS', payload: user });
+      else dispatch({ type: 'LOGOUT_SUCCESS' });
     });
-
+    
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
 
   return (
     <AuthContext.Provider value={state}>
---
+++ src/hooks/useAuthState.ts
@@ -0,0 +1,121 @@
+import { initialState, reducer } from '@/context/Auth/state';
+import type { Action, ActionsState, State } from '@/context/Auth/types';
+import { auth } from '@/firebase';
+import { googleProvider } from '@/firebase/providers';
+import {
+  checkUserDocExists,
+  createCredentialsUser,
+  createEmptyUserDoc,
+  signInCredentials,
+  signInPopup
+} from '@/util/user';
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { type Dispatch, useMemo, useReducer } from 'react';
+
+let r: any;
+export function useAuthState(): [State, ActionsState, Dispatch<Action>] {
+  const router = useRouter();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const actionsState = useMemo<ActionsState>(
+    () => ({
+      dispatch,
+      async signUp(email: string, password: string) {
+        dispatch({ type: 'LOADING' });
+        try {
+          const user = await createCredentialsUser(auth, email, password);
+          console.log('user: ' + user.emailVerified);
+          const isUserCreated = await createEmptyUserDoc(user);
+
+          if (!isUserCreated)
+            return dispatch({
+              type: 'REGISTER_ERROR',
+              payload: 'Failed to create user document.'
+            });
+
+          console.log('User document created successfully.');
+          router.prefetch('/dashboard');
+          router.push('/dashboard');
+        } catch (error) {
+          dispatch({
+            type: 'REGISTER_ERROR',
+            payload: (error as Error).message
+          });
+        }
+      },
+      async signIn(email: string, password: string) {
+        dispatch({ type: 'LOADING' });
+        try {
+          const user = await signInCredentials(auth, email, password);
+          console.log('user: ' + user.emailVerified);
+          const userExists = await checkUserDocExists(user.uid);
+
+          if (!userExists) {
+            const isUserCreated = await createEmptyUserDoc(user);
+
+            if (!isUserCreated)
+              return dispatch({
+                type: 'REGISTER_ERROR',
+                payload: 'Failed to create user document.'
+              });
+
+            dispatch({ type: 'REGISTER_SUCCESS', payload: user });
+          } else dispatch({ type: 'LOGIN_SUCCESS', payload: user });
+
+          router.prefetch('/dashboard');
+          router.push('/dashboard');
+        } catch (error) {
+          dispatch({
+            type: 'LOGIN_ERROR',
+            payload: (error as Error).message
+          });
+        }
+      },
+
+      async signInWithGoogle() {
+        dispatch({ type: 'LOADING' });
+        try {
+          const user = await signInPopup(auth, googleProvider);
+          const userExists = await checkUserDocExists(user.uid);
+
+          if (!userExists) {
+            const isUserCreated = await createEmptyUserDoc(user);
+
+            if (!isUserCreated)
+              return dispatch({
+                type: 'REGISTER_ERROR',
+                payload: 'Failed to create user document.'
+              });
+
+            dispatch({ type: 'REGISTER_SUCCESS', payload: user });
+          } else
+            dispatch({
+              type: 'LOGIN_SUCCESS',
+              payload: user
+            });
+
+          router.prefetch('/dashboard');
+          router.push('/dashboard');
+        } catch (error) {
+          dispatch({ type: 'LOGIN_ERROR', payload: (error as Error).message });
+        }
+      },
+
+      async signOut() {
+        dispatch({ type: 'LOADING' });
+        try {
+          await signOut(auth);
+          router.push('/');
+        } catch (error) {
+          dispatch({
+            type: 'LOGOUT_ERROR',
+            payload: (error as Error).message
+          });
+        }
+      }
+    }),
+    [dispatch, router]
+  );
+
+  return [state, actionsState, dispatch];
+}

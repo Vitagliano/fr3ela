096ee2736340eb9a04a491d128e8ed5be3ea5b9d
Igor Pereira <igor@gargantas.org>
2023-06-28 15:03:55 -0300

    [react-hook-form] feat: Adding fully featured and easy-to-use form components integrated with react-hook-form

--- src/components/Form/Field.tsx
+++ src/components/Form/Field.tsx
@@ -1,46 +1,61 @@
+"use client";
 /* eslint-disable react/display-name */
 import { PropsWithoutRef, ReactElement, createElement, memo } from "react";
-import { Controller, ControllerProps, FieldValues } from "react-hook-form";
+import {
+  FieldValues,
+  UseControllerProps,
+  UseControllerReturn,
+  useController
+} from "react-hook-form";
 import Input, { InputProps } from "../Input";
 
-type ControlRenderProps<F extends FieldValues> = Parameters<
-  ControllerProps<F>["render"]
->[0];
 type DerefInputProps = PropsWithoutRef<InputProps>;
-type RenderFunction = (props: DerefInputProps) => ReactElement;
+type RenderFunctionProps<F extends FieldValues> = DerefInputProps &
+  UseControllerReturn<F>;
+type RenderFunction<F extends FieldValues = FieldValues> = (
+  props: RenderFunctionProps<F>
+) => ReactElement;
 
-type FieldProps<F extends FieldValues> = Omit<ControllerProps<F>, "render"> &
-  Omit<InputProps, keyof ControllerProps> & {
-    render?: RenderFunction;
+type FieldProps<F extends FieldValues> = InputProps &
+  UseControllerProps<F> & {
+    render?: RenderFunction<F>;
   };
+
+function defaultRender<F extends FieldValues = FieldValues>({
+  field,
+  fieldState,
+  formState,
+  ...props
+}: RenderFunctionProps<F>) {
+  return <Input {...field} {...props} />;
+}
 
 function Field<F extends FieldValues>(props: FieldProps<F>) {
   const {
     name,
-    control,
     defaultValue,
     render,
     rules,
     shouldUnregister,
+    control,
     ...rest
   } = props;
 
-  const mountRender =
-    (fn: (p: DerefInputProps) => ReactElement) =>
-    (ctrlProps: ControlRenderProps<F>) =>
-      createElement(fn, { ...ctrlProps, ...rest, name });
+  const controller = useController({
+    name,
+    defaultValue,
+    control,
+    rules,
+    shouldUnregister
+  });
 
-  return (
-    <Controller
-      name={name}
-      control={control}
-      defaultValue={defaultValue}
-      rules={rules}
-      shouldUnregister={shouldUnregister}
-      render={mountRender(render ?? (Input as RenderFunction))}
-    />
-  );
+  return createElement(render ?? defaultRender, {
+    name,
+    ...rest,
+    ...controller
+  });
 }
 
-export default memo(Field) as typeof Field;
+const _Field = memo(Field) as typeof Field;
+export default _Field;
 export type { FieldProps };
